f9e1bffd668a6697499f3eaefc496cb428b7d53c
Georgiy <metallerok@gmail.com>
2023-01-22 18:09:33 +0300

    Note update service

--- src/services/notes/updater.py
+++ src/services/notes/updater.py
@@ -3,20 +3,21 @@ from typing import List
 from copy import deepcopy
 from src.message_bus.types import Event
 from src.message_bus import events
-from src.models.folder import Folder
+from src.models.note import Note
+
 from src.repositories.folders import FoldersRepoABC
 
 from uuid import UUID
 
 
-class FolderUpdateError(Exception):
+class NoteUpdateError(Exception):
     def __init__(self, message: str = None):
         self.message = message
 
         super().__init__()
 
 
-class FolderUpdaterABC(abc.ABC):
+class NoteUpdaterABC(abc.ABC):
     def __init__(self):
         self._events: List[Event] = []
 
@@ -27,16 +28,16 @@ class FolderUpdaterABC(abc.ABC):
     def update(
             self,
             data: dict,
-            folder: Folder,
+            note: Note,
             user_id: UUID,
-    ) -> Folder:
+    ) -> Note:
         raise NotImplementedError
 
 
-class FolderUpdater(FolderUpdaterABC):
+class NoteUpdater(NoteUpdaterABC):
     def __init__(
             self,
-            folders_repo: FoldersRepoABC,
+            folders_repo: FoldersRepoABC
     ):
         self._folders_repo = folders_repo
 
@@ -45,37 +46,37 @@ class FolderUpdater(FolderUpdaterABC):
     def update(
             self,
             data: dict,
-            folder: Folder,
+            note: Note,
             user_id: UUID,
-    ) -> Folder:
+    ) -> Note:
         data = deepcopy(data)
         updated_fields = {}
-        parent = None
+        folder = None
 
-        parent_id = data.pop("parent_id", None)
-        if parent_id:
-            parent = self._folders_repo.get(id_=parent_id, user_id=user_id)
+        folder_id = data.pop("folder_id", None)
+        if folder_id:
+            folder = self._folders_repo.get(id_=folder_id, user_id=user_id)
 
-            if parent is None:
-                raise FolderUpdateError(
-                    message=f"Parent folder (uuid={str(parent_id)}) not found"
+            if folder is None:
+                raise NoteUpdateError(
+                    message=f"Note update error. Folder (uuid={str(folder_id)}) not found"
                 )
 
-        if parent:
-            folder.parent_id = parent.id
-            updated_fields["parent_id"] = parent.id
+        if folder:
+            note.folder_id = folder.id
+            updated_fields["folder_id"] = folder.id
 
         for key, value in data.items():
-            if hasattr(folder, key):
+            if hasattr(note, key):
                 updated_fields[key] = value
-                setattr(folder, key, value)
+                setattr(note, key, value)
 
         self._events.append(
-            events.FolderUpdated(
+            events.NoteUpdated(
                 id=str(folder.id),
                 updated_fields=updated_fields,
                 user_id=str(user_id),
             )
         )
 
-        return folder
+        return note
--- tests/unit/test_notes_services.py
+++ tests/unit/test_notes_services.py
@@ -1,9 +1,16 @@
+import pytest
 from src.services.notes.creator import (
     NoteCreationInput,
     NoteCreator,
 )
 
+from src.services.notes.updater import (
+    NoteUpdater,
+    NoteUpdateError,
+)
+
 from src.repositories.notes import SANotesRepo
+from src.repositories.folders import SAFoldersRepo
 from src.models.primitives.note import (
     NoteTitle,
     NoteColor,
@@ -13,6 +20,7 @@ from src.message_bus import events
 
 from tests.helpers.users import make_test_user
 from tests.helpers.folders import make_test_folder
+from tests.helpers.notes import make_test_note
 
 from uuid import UUID
 
@@ -48,3 +56,68 @@ def test_note_creation_service(db_session):
     emitted_events = creator.get_events()
     emitted_events_types = [type(e) for e in emitted_events]
     assert events.NoteCreated in emitted_events_types
+
+
+def test_note_update_service(db_session):
+    user = make_test_user(db_session)
+    note = make_test_note(db_session, user)
+    folder = make_test_folder(db_session, user)
+
+    db_session.commit()
+
+    folders_repo = SAFoldersRepo(db_session)
+
+    updater = NoteUpdater(
+        folders_repo=folders_repo,
+    )
+
+    data = {
+        "title": NoteTitle("updated title"),
+        "color": NoteColor("#121212"),
+        "folder_id": UUID(folder.id),
+    }
+
+    note = updater.update(
+        data=data,
+        note=note,
+        user_id=UUID(user.id),
+    )
+
+    assert note
+    assert note.title == data["title"]
+    assert note.color == data["color"]
+    assert note.folder_id == folder.id
+
+    emitted_events = updater.get_events()
+    emitted_events_types = [type(e) for e in emitted_events]
+    assert events.NoteUpdated in emitted_events_types
+
+
+def test_try_update_note_with_wrong_folder(db_session):
+    user1 = make_test_user(db_session)
+    user2 = make_test_user(db_session)
+
+    note = make_test_note(db_session, user1)
+
+    wrong_folder = make_test_folder(db_session, user2)
+
+    db_session.commit()
+
+    folders_repo = SAFoldersRepo(db_session)
+
+    updater = NoteUpdater(
+        folders_repo=folders_repo,
+    )
+
+    data = {
+        "folder_id": UUID(wrong_folder.id)
+    }
+
+    with pytest.raises(NoteUpdateError) as e:
+        updater.update(
+            data=data,
+            note=note,
+            user_id=UUID(user1.id),
+        )
+
+    assert e.value.message == f"Note update error. Folder (uuid={str(wrong_folder.id)}) not found"
